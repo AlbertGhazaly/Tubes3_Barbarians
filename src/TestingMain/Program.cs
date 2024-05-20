9af41a97641954b35258a24ca4b0cdae53143b2e
hugosabam <hugo.sasianturi@gmail.com>
2024-05-20 14:15:27 +0700

    feat:Parser+TestMain project

---
+++ src/TestingMain/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using LogicLibrary.Parser;
+namespace TestMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = Directory.GetCurrentDirectory();
+            string directoryPath = "Sample/";
+            Console.WriteLine(rootDirectory);
+
+            // Instantiate and use the BitmapParser class
+            var bitmapParser = new BitmapParser(directoryPath);
+            bitmapParser.ParseAllAndPrintASCII();
+
+
+        }
+    }
+}
