9cf4797ea6529e5800cf2df3104d90d62fe7ddce
hugosabam <hugo.sasianturi@gmail.com>
2024-06-09 21:09:14 +0700

    add:exe release

--- src/LogicLibrary/obj/Release/net8.0/LogicLibrary.AssemblyInfo.cs
+++ src/LogicLibrary/obj/Release/net8.0/LogicLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c63e6802b9bbc59477a4d1754a6694f191c312f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb85c5c5b56794e0531f35b3a784a60246481395")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
